a90286aa76244a833bb1a96eff81eb0dae9c15a1
Caelan Reedy <rreedy@e1z2r3p18.42.us.org>
2018-07-12 16:48:35 -0700

    commit message

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,21 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 12:22:14 by rreedy            #+#    #+#             */
-/*   Updated: 2018/07/10 17:05:18 by rreedy           ###   ########.fr       */
+/*   Updated: 2018/07/12 16:46:18 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
 
-static char		*ft_realloc(char *s, int size)
+static char		*ft_realloc(char *s, int in, int size)
 {
 	char	*str;
 
 	str = ft_strnew(size);
-	ft_strncpy(str, s, size);
+	if (!str)
+		return (0);
+	ft_strncpy(str, s + in, size);
 	ft_strdel(&s);
 	return (str);
 }
@@ -36,45 +38,57 @@ static int		fill_line(char **line, char **str, int red)
 	if (!*line || red == -1)
 		return (-1);
 	*line = ft_strncpy(*line, *str, len);
-	if (ft_strlen(*str) - len)
-		*str = ft_strsub(*str, len + 1, BUFF_SIZE + (ft_strlen(*str) - len));
+	*str = ft_realloc(*str, len + 1, BUFF_SIZE + (ft_strlen(*str) - len));
+//	*str = ft_strsub(*str, len + 1, BUFF_SIZE + (ft_strlen(*str) - len));
 	return (*str ? 1 : -1);
 }
 
-static t_file	*newfile(int fd)
+static int		get_file(t_list **lst, int fd)
 {
 	t_file	*newf;
 
-	newf = (t_file *)ft_memalloc(sizeof(t_file));
-	if (!newf)
-		return (0);
-	newf->fide = fd;
-	newf->buf = ft_strnew(BUFF_SIZE);
-	return (newf);
+	if (!(*lst))
+		return (-1);
+	while ((*lst)->content && (((t_file *)((*lst)->content))->fide != fd))
+	{
+		if (!(*lst)->next)
+			(*lst)->next = ft_lstnew(0, 0);
+		*lst = (*lst)->next;
+	}
+	if (!((*lst)->content))
+	{
+		if (!(newf = (t_file *)ft_memalloc(sizeof(t_file))))
+			return (-1);
+		if (!(newf->buf = ft_strnew(BUFF_SIZE)))
+			return (-1);
+		newf->fide = fd;
+		(*lst)->content = newf;
+	}
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	static t_list	*lst;
+	static t_list	*head;
+	t_list			*lst;
 	int				red;
 	int				len;
 
 	red = BUFF_SIZE + 1;
 	if (fd < 0 || !line)
 		return (-1);
-	while (lst && lst->content && FD(lst) != fd)
-		lst = lst->next;
-	if (!lst)
-		lst = ft_lstnew(0, 0);
-	if (!lst->content)
-		lst->content = newfile(fd);
+	if (!head)
+		head = ft_lstnew(0, 0);
+	lst = head;
+	if (get_file(&lst, fd) == -1)
+		return (-1);
 	while (BUF(lst))
 	{
 		len = ft_strlen(BUF(lst));
 		if (ft_strchr(BUF(lst), '\n') || red < BUFF_SIZE)
 			return (fill_line(line, &BUF(lst), red));
 		if (BUFF_SIZE == (red = read(fd, BUF(lst) + len, BUFF_SIZE)))
-			BUF(lst) = ft_realloc(BUF(lst), ft_strlen(BUF(lst)) + BUFF_SIZE);
+			BUF(lst) = ft_realloc(BUF(lst), 0, ft_strlen(BUF(lst)) + BUFF_SIZE);
 	}
 	return (-1);
 }
