bdf3fd8c7ce6fc5bbebc142a4c0563e842538261
Caelan Reedy <rreedy@e1z2r3p18.42.us.org>
2018-07-14 13:10:41 -0700

    renaming

--- get_next_line.c
+++ get_next_line.c
@@ -6,14 +6,14 @@
 /*   By: rreedy <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/26 12:22:14 by rreedy            #+#    #+#             */
-/*   Updated: 2018/07/12 17:44:22 by rreedy           ###   ########.fr       */
+/*   Updated: 2018/07/14 13:02:10 by rreedy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <unistd.h>
 
-static char		*ft_realloc(char *s, int in, int size)
+static char		*stresize(char *s, int in, int size)
 {
 	char	*str;
 
@@ -38,7 +38,7 @@ static int		fill_line(char **line, char **s, int red)
 	if (!*line || red == -1)
 		return (-1);
 	*line = ft_strncpy(*line, *s, len);
-	*s = ft_realloc(*s, len + 1, BUFF_SIZE + (ft_strlen(*s) - len));
+	*s = stresize(*s, len + 1, BUFF_SIZE + (ft_strlen(*s) - len));
 	return (*s ? 1 : -1);
 }
 
@@ -71,7 +71,6 @@ int				get_next_line(const int fd, char **line)
 	static t_list	*head;
 	t_list			*lst;
 	int				red;
-	int				len;
 
 	red = BUFF_SIZE + 1;
 	if (fd < 0 || !line)
@@ -83,11 +82,11 @@ int				get_next_line(const int fd, char **line)
 		return (-1);
 	while (BUF(lst))
 	{
-		len = ft_strlen(BUF(lst));
 		if (ft_strchr(BUF(lst), '\n') || red < BUFF_SIZE)
 			return (fill_line(line, &BUF(lst), red));
-		if (BUFF_SIZE == (red = read(fd, BUF(lst) + len, BUFF_SIZE)))
-			BUF(lst) = ft_realloc(BUF(lst), 0, ft_strlen(BUF(lst)) + BUFF_SIZE);
+		red = read(fd, BUF(lst) + ft_strlen(BUF(lst)), BUFF_SIZE);
+		if (red == BUFF_SIZE)
+			BUF(lst) = stresize(BUF(lst), 0, ft_strlen(BUF(lst)) + BUFF_SIZE);
 	}
 	return (-1);
 }
